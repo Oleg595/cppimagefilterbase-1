6ab9fa3f8f1893f78894542216f42a82c86f0365
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2020-12-17 00:38:59 +0300

    Update png_toolkit.h

--- png_toolkit.h
+++ png_toolkit.h
@@ -3,8 +3,8 @@
 
 #include <string>
 #include <map>
-//#include <memory>
 #include "stb_image.h"
+#include"Filters2/Config.h"
 
 struct image_data
 {
@@ -27,10 +27,19 @@ public:
     bool load( std::string const &pictureName );
     bool save( std::string const &pictureName );
     image_data getPixelData( void ) const;
+    void Black_White(int start_x, int start_y, int finish_x, int finish_y);
+    void Red(int start_x, int start_y, int finish_x, int finish_y);
+    void Threshold(int start_x, int start_y, int finish_x, int finish_y);
+    void Edge(int start_x, int start_y, int finish_x, int finish_y);
+    void Blur(int start_x, int start_y, int finish_x, int finish_y);
+    stbi_uc Matrix5(int i, int j, int start_x, int start_y, int finish_x, int finish_y);
+    stbi_uc Matrix3_Edge(int i, int j, int start_x, int start_y, int finish_x, int finish_y);
+    stbi_uc Matrix3_Blur(int i, int j, int s, int start_x, int start_y, int finish_x, int finish_y);
+    void Paint(Config* conf);
+    void Paint_Filter(Filter filter);
 
 private:
     image_data imgData;
 };
 
 #endif // PNG_TOOLKIT_H
-
