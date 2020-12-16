4f865186d5da5f32a4830c671a3ff48fa226655a
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2020-12-17 00:58:42 +0300

    Update png_toolkit.h

--- png_toolkit.h
+++ png_toolkit.h
@@ -3,8 +3,9 @@
 
 #include <string>
 #include <map>
+#include<fstream>
 #include "stb_image.h"
-#include"Filters2/Config.h"
+using namespace std;
 
 struct image_data
 {
@@ -42,4 +43,32 @@ private:
     image_data imgData;
 };
 
+enum Fil_Name {
+    BLACK_WHITE,
+    RED,
+    THRESHOLD,
+    BLUR,
+    EDGE,
+    ERROR
+};
+
+struct Filter {
+    enum Fil_Name f_n;
+    int u;
+    int l;
+    int b;
+    int r;
+};
+
+class Config {
+public:
+    Config(string conf);
+    vector<Filter>* Fil();
+private:
+    vector<Filter>* filters;
+    string Read(string str, int* i);
+    enum Fil_Name Name(string str);
+};
+
 #endif // PNG_TOOLKIT_H
+
